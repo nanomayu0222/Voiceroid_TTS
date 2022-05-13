3f6016cfd7ffe775a233ea147948272b78ecab94
nanomayu0222 <favcreate86@gmail.com>
2022-05-13 20:03:27 +0900

    Change visibility(private->public).

--- Voiceroid_TTS/Form1.Designer.cs
+++ Voiceroid_TTS/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Voiceroid_TTS
 {
-    partial class Form1
+    partial class Form_Voiceroid_TTS
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -29,89 +29,44 @@ namespace Voiceroid_TTS
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_VR2_Play = new System.Windows.Forms.Button();
             this.txtBox_waitingTxt = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.Label_IsSpeakingStatus = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.Lavel_VR2_Status = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_VR2_Play
             // 
-            this.button1.Location = new System.Drawing.Point(12, 244);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(567, 70);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ｱｶﾘﾁｬﾝｶﾜｲｲﾔｯﾀｰ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Btn_VR2_Play.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_VR2_Play.Location = new System.Drawing.Point(12, 244);
+            this.Btn_VR2_Play.Name = "Btn_VR2_Play";
+            this.Btn_VR2_Play.Size = new System.Drawing.Size(450, 79);
+            this.Btn_VR2_Play.TabIndex = 0;
+            this.Btn_VR2_Play.Text = "ｱｶﾘﾁｬﾝｶﾜｲｲﾔｯﾀｰ";
+            this.Btn_VR2_Play.UseVisualStyleBackColor = true;
+            this.Btn_VR2_Play.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtBox_waitingTxt
             // 
+            this.txtBox_waitingTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtBox_waitingTxt.Location = new System.Drawing.Point(12, 12);
             this.txtBox_waitingTxt.Multiline = true;
             this.txtBox_waitingTxt.Name = "txtBox_waitingTxt";
-            this.txtBox_waitingTxt.Size = new System.Drawing.Size(567, 226);
+            this.txtBox_waitingTxt.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtBox_waitingTxt.Size = new System.Drawing.Size(447, 223);
             this.txtBox_waitingTxt.TabIndex = 1;
+            this.txtBox_waitingTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBox_waitingTxt_KeyDown);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F);
-            this.label1.Location = new System.Drawing.Point(20, 330);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 28);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Status:";
-            // 
-            // Label_IsSpeakingStatus
-            // 
-            this.Label_IsSpeakingStatus.AutoSize = true;
-            this.Label_IsSpeakingStatus.Font = new System.Drawing.Font("MS UI Gothic", 14F);
-            this.Label_IsSpeakingStatus.ForeColor = System.Drawing.Color.Green;
-            this.Label_IsSpeakingStatus.Location = new System.Drawing.Point(128, 331);
-            this.Label_IsSpeakingStatus.Name = "Label_IsSpeakingStatus";
-            this.Label_IsSpeakingStatus.Size = new System.Drawing.Size(117, 28);
-            this.Label_IsSpeakingStatus.TabIndex = 2;
-            this.Label_IsSpeakingStatus.Text = "<Status>";
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.label3.Location = new System.Drawing.Point(21, 375);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(162, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Voiceroid2 Status:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // Lavel_VR2_Status
-            // 
-            this.Lavel_VR2_Status.AutoSize = true;
-            this.Lavel_VR2_Status.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Lavel_VR2_Status.Location = new System.Drawing.Point(189, 375);
-            this.Lavel_VR2_Status.Name = "Lavel_VR2_Status";
-            this.Lavel_VR2_Status.Size = new System.Drawing.Size(84, 20);
-            this.Lavel_VR2_Status.TabIndex = 3;
-            this.Lavel_VR2_Status.Text = "<Status>";
-            this.Lavel_VR2_Status.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // Form1
+            // Form_Voiceroid_TTS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(591, 424);
-            this.Controls.Add(this.Lavel_VR2_Status);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Label_IsSpeakingStatus);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(478, 333);
             this.Controls.Add(this.txtBox_waitingTxt);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.Btn_VR2_Play);
+            this.Name = "Form_Voiceroid_TTS";
+            this.Text = "Voiceroid_TTS";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,12 +74,8 @@ namespace Voiceroid_TTS
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_VR2_Play;
         private System.Windows.Forms.TextBox txtBox_waitingTxt;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label Label_IsSpeakingStatus;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label Lavel_VR2_Status;
     }
 }
 
--- Voiceroid_TTS/Form1.cs
+++ Voiceroid_TTS/Form1.cs
@@ -2,20 +2,23 @@
 using System.Drawing;//LabelコンポーネントのForeColorを変更するのに必要
 using System.Windows.Forms;
 using Voiceroid2_Driver;//外部ファイルのクラスを使用するときに使用？？
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Voiceroid_TTS
 {
-    public partial class Form1 : Form
+    public partial class Form_Voiceroid_TTS : Form
     {
-        public Form1()
+        public Form_Voiceroid_TTS()
         {
             InitializeComponent();
+            //Size = new Size(300, 190);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Label_IsSpeakingStatus.Text = "Speaking";
-            Label_IsSpeakingStatus.ForeColor = Color.Red;
+
+            Btn_VR2_Play.Enabled = false;
+
             string inputedText = txtBox_waitingTxt.Text;
 
             //Vroid2_akari.Vroid2_Speak(inputedText);
@@ -25,18 +28,55 @@ namespace Voiceroid_TTS
             VR2_Driver driver = new VR2_Driver();
             if (driver.Play(inputedText) == -1.0)
             {
-                Lavel_VR2_Status.Text = "Not runnning";
-                Lavel_VR2_Status.ForeColor = Color.Red;
+                // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+                new ToastContentBuilder()
+                    .AddArgument("action", "viewConversation")
+                    .AddArgument("conversationId", 9813)
+                    .AddText("Voiceroid2が起動していません。")
+                    .AddText("Notifcation Visualiser")
+                    .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 5, your TFM must be net5.0-windows10.0.17763.0 or greater
+            
             }
 
             txtBox_waitingTxt.Text = "";
-            Label_IsSpeakingStatus.Text = "Ready";
-            Label_IsSpeakingStatus.ForeColor = Color.Green;
 
+            Btn_VR2_Play.Enabled = true;
             //TODO
             //デザイン向上
             //自動起動
-            //Voiceroid本体の起動確認←Windowsトーストでできない？（そもそもC#？）
+        }
+
+        private void txtBox_waitingTxt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (e.Shift)
+                {
+                    
+                }
+                else
+                {
+                    string inputedText = txtBox_waitingTxt.Text;
+
+                    //Vroid2_akari.Vroid2_Speak(inputedText);
+
+                    //再生開始・終了の判別を再生ボタンの画像の内容で行う
+                    //https://hgotoh.jp/wiki/doku.php/documents/voiceroid/assistantseika/assistantseika-093
+                    VR2_Driver driver = new VR2_Driver();
+                    if (driver.Play(inputedText) == -1.0)
+                    {
+                        // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+                        new ToastContentBuilder()
+                            .AddArgument("action", "viewConversation")
+                            .AddArgument("conversationId", 9813)
+                            .AddText("Voiceroid2が起動していません。")
+                            .AddText("Notifcation Visualiser")
+                            .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 5, your TFM must be net5.0-windows10.0.17763.0 or greater
+                    }
+                }
+            }
+
+            txtBox_waitingTxt.Text = "";
         }
     }    
 }
